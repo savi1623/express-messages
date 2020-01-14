ee49fe31c5d295d7125054f6d1005047ec9255a1
Savannah Williams <Savi@YeeHaw.local>
2020-01-14 10:32:59 -0700

    Adding npm and express to app

--- db/config.js
+++ db/config.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = 3000;
+
 module.exports = mongoose.connect('mongodb://localhost:27017/messages', {useNewUrlParser: true, useUnifiedTopology: true});
+
+app.get('/', (req,res) => res.send('Server Up'));
+
+app.listen(port, () => console.log('server up on port: ' + port));
