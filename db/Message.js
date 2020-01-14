9c3926e9fd260bb23cf32e474badfa5f881fee93
Savannah Williams <Savi@YeeHaw.local>
2020-01-14 15:55:21 -0700

    Attempt to render the server

--- db/Message.js
+++ db/Message.js
@@ -1,7 +1,16 @@
 //Message Model
 const db = require('./config');
+const mongoose = require('mongoose');
 
-const Message = null;
 
+let messageSchema = new mongoose.Schema({
+  name: String,
+  message: String
+});
+
+let Message = mongoose.model('Message', messageSchema);
+
+
+//equals model
 
 module.exports = Message;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const Message = require('./db/Message');
 
+app.use(bodyParser.json());
 app.use((req,res,next) => {
   res.status(404).send('That route does not exist');
 });
@@ -10,6 +12,37 @@ const port = 3000;
 
 app.listen(port, () => {
   console.log('Listening on port', port);
+});
+
+//where to write app.post routes and app.get routes using class that is Message
+//for app/message
+
+app.post('/messages', (res, req) => {
+  //for the create
+  Message.create({name, message})
+    .then(() => {
+  res.status(201);
+  res.send('Made Post Request')})
+});
+
+app.get('/messages', (res, req) => {
+  //for the find or find1
+  Message.find();
+});
+
+app.get('/messages' + Message.id, (res, req) => {
+  //for the find or find1
+  Message.findOne();
+});
+
+app.put('/messages'+ Message.id, (res, req) => {
+  //for the update
+  Message.update();
+});
+
+app.delete('/messages'+ Message.id, (res, req) => {
+  //for the delete
+  Message.findOneAndDelete();
 });
 
 module.exports = app;
